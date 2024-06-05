cc7a18fb115c607354f2e41f17c73db5e66d1d7e
Miroslav Simko <ms@iolabs.ch>
2024-06-05 18:54:24 +0900

    human readable

--- file_sizes.ps1
+++ file_sizes.ps1
@@ -2,8 +2,19 @@ $folders = Get-ChildItem -Directory
 
 foreach ($folder in $folders) {
     $size = Get-ChildItem $folder.FullName -Recurse | Measure-Object -Property Length -Sum
-    $sizeInKB = $size.Sum / 1KB
-    Write-Output "$($folder.Name): $($sizeInKB.ToString("N2")) KB"
+    $sizeInBytes = $size.Sum
+    $sizeReadable = if ($sizeInBytes -ge 1TB) {
+        "{0:N2} TB" -f ($sizeInBytes / 1TB)
+    } elseif ($sizeInBytes -ge 1GB) {
+        "{0:N2} GB" -f ($sizeInBytes / 1GB)
+    } elseif ($sizeInBytes -ge 1MB) {
+        "{0:N2} MB" -f ($sizeInBytes / 1MB)
+    } elseif ($sizeInBytes -ge 1KB) {
+        "{0:N2} KB" -f ($sizeInBytes / 1KB)
+    } else {
+        "{0:N2} bytes" -f $sizeInBytes
+    }
+    Write-Output "$($folder.Name): $($sizeReadable)"
 }
 
 pause
