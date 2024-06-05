b45c6ac20fddd7d46eef7a2999a69529b0b8a392
Miroslav Simko <ms@iolabs.ch>
2024-06-05 14:14:47 +0900

    human-readable format

--- file_sizes.bat
+++ file_sizes.bat
@@ -1,12 +1,24 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+rem Function to format bytes to human-readable format
+:formatSize
+set size=%1
+set suffix=B
+if %size% GEQ 1024 set size=!size:~0,-3!.K& set suffix=KB
+if %size% GEQ 1024 set size=!size:~0,-3!.M& set suffix=MB
+if %size% GEQ 1024 set size=!size:~0,-3!.G& set suffix=GB
+echo %size% %suffix%
+exit /b
+
+rem Loop through each directory in the current folder
 for /d %%i in (*) do (
-    echo %%i
-    for /f "usebackq tokens=3" %%j in (`dir /s /-c "%%i" ^| find "File(s)"`) do echo %%j
-)
-for %%i in (*) do (
-    if not "%%~xi"=="" (
-        echo %%i
-        for /f "usebackq tokens=3" %%j in (`dir /s /-c "%%i" ^| find "File(s)"`) do echo %%j
+    set size=0
+    for /f "tokens=3" %%j in ('dir /s /a /-c "%%i" ^| find "File(s)"') do (
+        set /a size+=%%j
     )
+    echo %%i
+    call :formatSize !size!
 )
+
 pause
